8658f1d719b680ab944909bbbcc2acef43ad11e6
Ben Siver <bsiver@gmail.com>
2011-08-12 11:19:05 -0500

    Solution for problem 2 in haskell using a list comprehension

---
+++ p2.hs
@@ -0,0 +1,10 @@
+fib 0 = 0
+fib 1 = 1
+fib n | even n         = f1 * (f1 + 2 * f2)
+      | n `mod` 4 == 1 = (2 * f1 + f2) * (2 * f1 - f2) + 2
+      | otherwise      = (2 * f1 + f2) * (2 * f1 - f2) - 2
+   where k = n `div` 2
+         f1 = fib k
+         f2 = fib (k-1)
+
+sum(filter even [ (fib x) | x <- [0..33]])
