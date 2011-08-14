87ab88413fa96164ac6ab677322f3d0cd5b981ea
ben <ben@colors.(none)>
2011-08-13 22:04:02 -0500

    Utility class for common functions

---
+++ util.hs
@@ -0,0 +1,14 @@
+isPrime x = not $ any divisible $ takeWhile notTooBig [2..] where
+     divisible y = x `mod`y == 0
+     notTooBig y = y*y <= x
+
+divisors n = 1 : filter ( (==0) . rem n) [2..n `div` 2]
+
+fib 0 = 0
+fib 1 = 1
+fib n | even n         = f1 * (f1 + 2 * f2)
+      | n `mod` 4 == 1 = (2 * f1 + f2) * (2 * f1 - f2) + 2
+      | otherwise      = (2 * f1 + f2) * (2 * f1 - f2) - 2
+   where k = n `div` 2
+         f1 = fib k
+         f2 = fib (k-1)
